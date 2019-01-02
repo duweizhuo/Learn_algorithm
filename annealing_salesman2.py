fcebfa9c40ff2bb6da70b850facd38b12682d382
duweizhuo <duweizhuo@21cn.com>
2019-01-03 01:23:51 +0800

    Update annealing_salesman2.py

--- annealing_salesman2.py
+++ annealing_salesman2.py
@@ -7,4 +7,21 @@ s_new = s_old
 
 while(tmp > tmp_min):
     cit_new = new_solve(cities1)
+    s_new = dist(cit_new)
+    dE = (s_new-s_old) * 500
+    
+    if(dE > tmp):
+        cities1 = cit_new
+        s_old = s_new
+        
+    if(dE < 0):
+        counter = 0
+        tmp = tmp * alpha
+    else:
+        counter = counter + 1
+        
+    if(counter > 1e4):
+        break
+def draw-route(in):
+def dist():
     
