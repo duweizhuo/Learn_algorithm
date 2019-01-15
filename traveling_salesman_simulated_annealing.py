28c19f381f3a34ecfce45cdb1dd5ccd43e6f1058
duweizhuo <duweizhuo@21cn.com>
2019-01-15 22:08:22 +0800

    Update traveling_salesman_simulated_annealing.py

--- traveling_salesman_simulated_annealing.py
+++ traveling_salesman_simulated_annealing.py
@@ -1,33 +1,25 @@
 import random, numpy, math, copy, matplotlib.pyplot as plt
-cities = [random.sample(range(100), 2) for x in range(15)]
-tour = random.sample(range(15), 15)
+cities = [[0, 84], [45, 53], [17, 53], [89, 5], [24, 66], [98, 35], [38, 18], [90, 89], [8, 12], [96, 47]]
+tour = random.sample(range(10),10)
 
-def Distance(city1, city2):
-    xDis = abs(city1[0] - city2[0])
-    yDis = abs(city1[1] - city2[1])
-    distance = math.sqrt((xDis**2) + (yDis**2))
+def dist(city1, city2):
+    xDist = abs(city1[0] - city2[0])
+    yDist = abs(city1[1] - city2[1])
+    distance = math.sqrt(xDist**2 + yDist**2)
     return distance
-    
-def DistanceChange(tours, new_way):
-    DistanceChange = 0
+
+def distChange(tour, new_way):
+    distanceChange = 0
     for k in new_way:
-        DistanceChange += Distance(cities[tours[(k+1) % 15]], cities[tours[k % 15]]) 
-#    print(DistanceChange)
-    return DistanceChange
-    
-    
-    
-#def delta_E() 
-for temperature in numpy.logspace(0, 5, num = 100000)[::-1]:
-    [i, j] = sorted(random.sample(range(15), 2))
-    newTour = copy.copy(tour)
-    newTour[j], newTour[i] = newTour[i], newTour[j] 
-#    newTour = tour[:i] + tour[j:j+1] + tour[i+1:j] + tour[i:i+1] + tour[j+1:]
+        distanceChange += dist(cities[tour[(k+1)%10]], cities[tour[k]])
+    return distanceChange
 
-    P = math.exp((DistanceChange(tour,[j, j-1, i, i-1]) - DistanceChange(newTour,[j, j-1, i, i-1])) / temperature)
-
-
+for temp in numpy.logspace(0, 5, num = 100000)[::-1]:
+    [i, j] = sorted(random.sample(range(10), 2))
+    newTour = copy.deepcopy(tour)
+    newTour[i], newTour[j] = newTour[j], newTour[i]
+    P = math.exp((distChange(tour,[j, j-1, i, i-1])) - distChange(newTour, [j, j-1, i, i-1]) / temp)
+    
     if P > random.random():
-        tour = copy.copy(newTour);
-plt.plot([cities[tour[i % 15]][0] for i in range(16)], [cities[tour[i % 15]][1] for i in range(16)], 'xb-')
-plt.show()
+        tour = copy.copy(newTour)
+plt.plot([cities[tour[i%10]][0] for i in range(11)], [cities[tour[i%10]][1] for i in range(11)], 'xb-')
